fe8c48c70c3fb3a806b744b12462f7d5df6de63a
Sebastian Oeltjen <sloeltjen@go.stcloudstate.edu>
2019-11-02 15:43:29 -0500

    remove django-google-maps dependency

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_google_maps import fields as map_fields
 
 # Create your models here.
 class Item(models.Model):
@@ -27,8 +26,6 @@ class Comment(models.Model):
     date = models.DateField(auto_now_add=True)
     time = models.TimeField(auto_now_add=True)
 
-    address = map_fields.AddressField(max_length=200)
-    geolocation = map_fields.GeoLocationField(max_length=100)
 
 class User(models.Model):
     name = models.CharField(max_length=30)
