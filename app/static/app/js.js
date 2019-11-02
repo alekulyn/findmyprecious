1615db5e0cafea1d88089ae0e8162eaa8cc0ddac
alekulyn <alekulyn@users.noreply.github.com>
2019-11-02 11:53:42 -0500

    Basic signup and login

--- app/static/app/js.js
+++ app/static/app/js.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
+	//csrftoken = $.cookie('csrftoken');
+
 	$('#su_s').on('click', function () {
 		var user = $('#su_u').val();
 		var pass = $('#su_p').val();
 		$.ajax({
-			type:"GET",
+			type:"POST",
 			url: "signup",
 			data: {
 				user: user,
 				pass: pass
 			},
 			success: function (data) {
+				//data is the success message
+				$('#message').show();
+				$('#message').text('Message: Login created. Now please sign in.');
 				return;
 			}
 		})
@@ -19,14 +24,21 @@ $(document).ready(function() {
 		var user = $('#si_u').val();
 		var pass = $('#si_p').val();
 		$.ajax({
-			type:"GET",
+			type:"POST",
 			url: "signin",
 			data: {
 				user: user,
 				pass: pass
 			},
 			success: function (data) {
-				
+				if (data == "success") {
+					$('#message').show();
+					$('#message').text('Message: Login successful. Thank you.');
+				} else {
+					$('#message').show();
+					$('#message').text('Message: Login failed. Please try again.');
+				}
+				//Data could be success or failure
 			}
 		})
 	});
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth import authenticate, login
 
 from django.contrib.auth.models import User
 from .models import Item
@@ -12,22 +13,24 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def signup(request):
-    username = request.GET['user']
-    password = request.GET['pass']
+    username = request.POST['user']
+    password = request.POST['pass']
     
     new_user = User.objects.create_user(username=username, password=password)
     new_user.save()
+    return HttpResponse("success")
 
 def signin(request):
-    username = request.GET['user']
-    password = request.GET['pass']
+    username = request.POST['user']
+    password = request.POST['pass']
 
     user = authenticate(username=username, password=password)
 
     if user is not None:
-        return HttpResponse("Success")
+        login(request, user)
+        return HttpResponse("success")
     else:
-        return HttpResponse("Failure")
+        return HttpResponse("failure")
 
 def addmarker(request):
     user = User.object() #Get current user
