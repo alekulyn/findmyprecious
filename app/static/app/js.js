f90bf46e664a4b66d7e944ce6943b7f3c78e38de
alekulyn <alekulyn@users.noreply.github.com>
2019-11-02 20:22:27 -0500

    Colored map marker, better info windows, and logout functionality

--- app/static/app/js.js
+++ app/static/app/js.js
@@ -1,3 +1,13 @@
+blue_icon = {
+	      url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+}
+red_icon = {
+	      url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+}
+green_icon = {
+	      url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+}
+
 function refresh_map () {
 	$.ajax({
 		type:"POST",
@@ -5,14 +15,29 @@ function refresh_map () {
 		success: function (data) {
 			if (data.status == "success") {
 				data.pins = JSON.parse(data.pins)
-				console.log(data);
 				//fetch pins from data.pins and display them
 				for (var i = 0; i < data.pins.length; i++) {
 					var pt_lat_lng = new google.maps.LatLng(data.pins[i].latitude, data.pins[i].longitude);
 					var loc = new google.maps.Marker({
-						    position: pt_lat_lng,
-						    title: data.pins[i].title
+						position: pt_lat_lng,
+						title: data.pins[i].title,
+						icon: ((data.pins[i].found) ? green_icon : red_icon)
 					});
+
+					google.maps.event.addListener(loc, 'click', (function(loc, i) {
+						return function () {
+							if (infoWindow) {
+								infoWindow.close()
+							}
+							var iw = new google.maps.InfoWindow({
+								content: '<b>Reporter:</b> ' + data.pins[i].user + '<br>' +
+									 '<b>Title:</b> ' + data.pins[i].title + '<br>' +
+									 '<b>Description:</b> ' + data.pins[i].desc + '<br>'
+							});
+							infoWindow = iw;
+							iw.open(map,loc);
+						}
+					})(loc, i));
 					loc.setMap(map);
 				}
 			}
@@ -63,12 +88,26 @@ $(document).ready(function() {
 			}
 		})
 	});
+
+	$('#logout').on('click', function () {
+		$.ajax({
+			type:"POST",
+			url: "logout",
+			success: function (data) {
+				if (data == "success") {
+					$('#message').text('Message: Logout successful. Thank you.');
+					$('#currentuser').text('Logged in as: guest');
+				}
+			}
+		})
+	});
 	
 	$('#ap_s').on('click', function () {
 		var title = $('#title').val();
 		var desc = $('#desc').val();
 		var lat = marker.position.lat;
 		var lng = marker.position.lng;
+		var found = $('input[name=itemtype]:checked').val() == "found";
 		$.ajax({
 			type:"POST",
 			url: "addmarker",
@@ -76,12 +115,19 @@ $(document).ready(function() {
 				title: title,
 				desc: desc,
 				latitude: lat,
-				longitude: lng
+				longitude: lng,
+				found: found
 			},
 			success: function (data) {
 				if (data == "success") {
 					//refresh map
 					refresh_map();
+					//clear temporary pin and pin fields
+					marker.setMap(null);
+					marker = null;
+					$('title').val('');
+					$('desc').val('');
+					$('input[name=itemtype]').prop('checked', false);
 				}
 			}
 		})
